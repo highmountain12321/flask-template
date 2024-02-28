3fb5e45a9fc2066d441354e08ec5ffaa1ddd25ae
highmountain12321 <timesaver1006@gmail.com>
2024-02-28 14:54:15 +0200

    feat: new template

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import pandas as pd
+import markdown
+
+app = Flask(__name__)
+
+def read_excel():
+    df = pd.read_excel("./fakedb/Publications example v7.0 29Nov23.xlsx")
+    # Assuming 'abstract' column contains Markdown content
+    df['abstract'] = df['abstract'].astype(str).apply(lambda x: markdown.markdown(x)).apply(lambda x: x.replace('\n', '<br>'))
+    return df
+
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/table')
+def show_table():
+    dataframe = read_excel()
+    return render_template('table_view.html', dataframe=dataframe)
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0", port=5001)
+
